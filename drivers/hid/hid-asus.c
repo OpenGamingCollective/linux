500083b16247ae64b40759978a6c985537672e3a
Luke D. Jones <luke@ljones.dev>
2024-08-26 12:49:35 +1200

    [FOR-UPSTREAM] hid-asus-ally: Add joystick LED ring support
    Adds basic support for the joystick RGB LED rings as a multicolour LED
    device with 4 LEDs. As this uses the software-mode for setting the LED
    colours it also sets the MCU-mode for LED's to static + the first RGB
    colour on suspend/reboot/shutdown. This is done to prevent user confusion
    if the LED's were to not match what was set, and to maintain consistency.
    
    Signed-off-by: Luke D. Jones <luke@ljones.dev>

--- drivers/hid/hid-asus.c
+++ drivers/hid/hid-asus.c
@@ -32,6 +32,7 @@
 #include <linux/leds.h>
 
 #include "hid-ids.h"
+#include "hid-asus.h"
 
 MODULE_AUTHOR("Yusuke Fujimaki <usk.fujimaki@gmail.com>");
 MODULE_AUTHOR("Brendan McGrath <redmcg@redmandi.dyndns.org>");
@@ -1195,6 +1196,8 @@ static int asus_probe(struct hid_device *hdev, const struct hid_device_id *id)
 {
 	struct hid_report_enum *rep_enum;
 	struct asus_drvdata *drvdata;
+	struct usb_host_endpoint *ep;
+	struct usb_interface *intf;
 	struct hid_report *rep;
 	bool is_vendor = false;
 	int ret;
@@ -1208,6 +1211,18 @@ static int asus_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	hid_set_drvdata(hdev, drvdata);
 
 	drvdata->quirks = id->driver_data;
+
+	/* Ignore these endpoints as they are used by hid-asus-ally */
+	#if IS_REACHABLE(CONFIG_HID_ASUS_ALLY)
+	if (drvdata->quirks & QUIRK_ROG_ALLY_XPAD) {
+		intf = to_usb_interface(hdev->dev.parent);
+		ep = intf->cur_altsetting->endpoint;
+		if (ep->desc.bEndpointAddress == ROG_ALLY_X_INTF_IN ||
+			ep->desc.bEndpointAddress == ROG_ALLY_CFG_INTF_IN ||
+			ep->desc.bEndpointAddress == ROG_ALLY_CFG_INTF_OUT)
+			return -ENODEV;
+	}
+	#endif /* IS_REACHABLE(CONFIG_HID_ASUS_ALLY) */
 
 	/*
 	 * T90CHI's keyboard dock returns same ID values as T100CHI's dock.
